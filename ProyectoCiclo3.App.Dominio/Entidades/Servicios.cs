7e2692136f6e42a0448fb27d776d1f767fed44f9
Sebast-Git <daniel.burbano@correounivalle.edu.co>
2021-09-21 20:29:50 -0500

    Subida1

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Servicios.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Servicio{
+    public int id { get; set; }
+    public string origen { get; set; }
+    public string destino { get; set; }
+    public string fecha { get; set; }
+    public string hora { get; set; }
+    public string encomienda { get; set; }
+    
+    }
+
+}
---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoCiclo3.App.Dominio;
+ 
+namespace ProyectoCiclo3.App.Persistencia
+{
+    public class AppContext: DbContext{
+        public DbSet<Encomienda> Encomiendas { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Servicio> Servicio { get; set; }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ProyectoCiclo3");
+            }
+        }
+    }
+}
+
